a9c12b500fb689cdd95e86cf6b7ac301320572ab
trash80.v2.0 <trash80.v2.0@83da872c-a47e-11dd-bd46-993b494de23d>
2011-06-25 04:09:29 +0000

    some fun with modules in python.  Also creating individual modules for use in xiin.  Final xiin will be self contained.  But modules are experimental uses.

---
+++ modules/trash80-experimental/modules/xiin/PythonVersionCheck.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+#    author: Scott Rogers
+#    stability: alpha
+#    copying: 'Copyright (C) 2011 W. Scott Rogers
+#              This program is free software.
+#              You can redistribute it and/or modify it under the terms of the
+#              GNU General Public License as published by the Free Software Foundation;
+#              version 2 of the License.
+#
+
+import sys
+
+class PythonVersionCheck(object):
+    # http://stackoverflow.com/questions/1093322/how-do-i-check-what-version-of-python-is-running-my-script
+
+    def __init__(self):
+        """
+        """
+        self = self
+        self.lowestVersion = None
+    #end
+
+    def check(self):
+            """ Detects Python compatibility."""
+
+            pythonVersionText = 'Detecting Python version...[version 2.6+ required]...'
+            pythonVersionErrorText = 'ERROR: Incorrect Python version: 2.6+ is required'
+            pythonVersionPassText = 'Passed...continuing'
+
+            print('')
+            print(pythonVersionText)
+
+            if (self.lowestVersion is None):
+                self.setMinimumVersion()
+
+            if sys.hexversion < self.lowestVersion:
+                print('')
+                print(pythonVersionErrorText)
+                exit(1)
+            else:
+                print(pythonVersionPassText)
+                print('')
+                return
+        #end
+
+    def setMinimumVersion(self, lowestVersion = 0x02060000):
+        """
+        lowestVersion:  The minimum python version required. [Default: 2.6 ]
+        """
+        self.lowestVersion = lowestVersion
+#end
+
+if __name__ == '__main__':
+    version = PythonVersionCheck()
+    version.check()
+#end
---
+++ modules/trash80-experimental/modules/xiin/xiinSpin.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+#    author: Scott Rogers
+#    stability: alpha
+#    copying: 'Copyright (C) 2011 W. Scott Rogers
+#              This program is free software.
+#              You can redistribute it and/or modify it under the terms of the
+#              GNU General Public License as published by the Free Software Foundation;
+#              version 2 of the License.
+#
+# [ ' [\\] ', ' [|] ', ' [/] ', ' [-] ']
+
+import sys
+import time
+
+class Spinner(object):
+
+    def __init__(self, typeOfSpinner = [ ' [\\] ', ' [|] ', ' [/] ', ' [-] '], color = None):
+        """
+        typeOfSpinner:  A dictionary of characters to use as the spinner.
+        color:          The color of the spinner. Supports ASCII colors. [Default: stdio default ]
+        """
+        self = self
+        self.typeOfSpinner  = typeOfSpinner
+        self.color          = color
+        self.mod            = 0
+    #end
+
+    def render(self, count):
+        """ Displays a busy spinner."""
+
+        spinner = self.typeOfSpinner
+        counter = (((count - 1)/4)%4)
+
+        if (counter == self.mod):
+            self.mod = self.mod + 1
+            if self.mod > 3:
+                self.mod = 0
+            print(spinner[self.mod]),
+            sys.stdout.flush()
+            sys.stdout.write('\r')
+
+    #end
+
+    def setSpinnerImage(self, typeOfSpinner):
+        """
+        typeOfSpinner: A dictionary of characters to use as the spinner.
+        """
+        self.typeOfSpinner = typeOfSpinner
+    #end
+
+    def setSpinnerColor(self, color):
+        """
+        color: The color of the spinner. Supports ASCII colors. [Default: stdio default ]
+        """
+        self.color = color
+    #end
+
+    def getSpinnerImage(self):
+        return self.typeOfSpinner
+    #end
+
+    def getSpinnerColor(self):
+        return self.color
+    #end
+
+#end
+
+if __name__ == '__main__':
+    spinner = Spinner()
+
+    for i in range(100):
+        spinner.render(i)
+        time.sleep(0.05)
+#end
